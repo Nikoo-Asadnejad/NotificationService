88d878e60db40e96621ea64c8f63f0b3400a61c3
Nikoo Asadnejad <nikoo.a.14@gmail.com>
2023-08-30 23:25:49 +0330

    feat(smsService) : add sms consumer

---
+++ src/SmsService/Consumers/SmsConsumer.cs
@@ -0,0 +1,12 @@
+using MassTransit;
+using SmsContract.Models;
+
+namespace SmsService.Consumers;
+
+public class SmsConsumer : IConsumer<SendSmsRequest>
+{
+    public Task Consume(ConsumeContext<SendSmsRequest> context)
+    {
+        throw new NotImplementedException();
+    }
+}
--- src/SmsService/Program.cs
+++ src/SmsService/Program.cs
@@ -1,4 +1,6 @@
+using MassTransit;
 using SmsService.Configurations;
+using SmsService.Consumers;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,7 +8,25 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.InjectService(builder.Configuration);
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 app.ConfigurePipeline();
+
+var busControl = Bus.Factory.CreateUsingRabbitMq(cfg =>
+{
+    cfg.ReceiveEndpoint("send-email-event", e =>
+    {
+        e.Consumer<SmsConsumer>();
+    });
+});
+
+await busControl.StartAsync(new CancellationToken());
+try
+{
+    Console.WriteLine("Press enter to exit");
+    await Task.Run(() => Console.ReadLine());
+}
+finally
+{
+    await busControl.StopAsync();
+}
+
 app.Run();
