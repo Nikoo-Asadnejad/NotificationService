193469fc09324d130c81a27b4602cd4c23bff859
Nikoo Asadnejad <nikoo.a.14@gmail.com>
2023-08-31 11:35:48 +0330

    feat : add push service

---
+++ src/Services/PushService/Configurations/Configuration.cs
@@ -0,0 +1,41 @@
+namespace PushService.Configurations;
+
+public struct Configuration
+{
+    public static AppSetting AppSetting { get;}
+}
+public struct AppSetting
+{
+    public Logging Logging { get; set; }
+    public MailSettings MailSettings { get; set; }
+    
+    public RabbitMqSetting RabbitMqSetting { get; set; }
+}
+
+public struct Logging
+{
+    public LogLevel LogLevel { get; set; }
+}
+
+public struct LogLevel
+{
+    public string Default { get; set; }
+    public string Microsoft_AspNetCore { get; set; }
+}
+
+public record MailSettings
+{
+    public string Server { get; set; }
+    public int Port { get; set; }
+    public string SenderName { get; set; }
+    public string SenderEmail { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+}
+
+public struct RabbitMqSetting
+{
+    public  string Url { get; set; }
+    public  string UserName { get; set; }
+    public  string Password { get; set; }
+}
---
+++ src/Services/PushService/Configurations/Configurator.cs
@@ -0,0 +1,38 @@
+using MassTransit;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+namespace PushService.Configurations;
+
+public static class SmsConfigurator
+{
+    public static void InjectService(this IServiceCollection services , IConfiguration configuration)
+    {
+
+        services.AddMassTransit(configurator =>
+        {
+            configurator.UsingRabbitMq();
+        });
+        configuration.Bind(Configuration.AppSetting);
+    }
+    
+    public static void AddRabbitConsumer(this IServiceCollection services)
+        =>services.AddMassTransit(configurator =>
+        {
+            var rabbitSetting = Configuration.AppSetting.RabbitMqSetting;
+            configurator.UsingRabbitMq();
+
+        });
+
+    public static void ConfigurePipeline(this WebApplication app)
+    {
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapHealthChecks("/health/ready", new HealthCheckOptions()
+            {
+                Predicate = (check) => check.Tags.Contains("ready"),
+            });
+
+            endpoints.MapHealthChecks("/health/live", new HealthCheckOptions());
+        });
+    }
+}
