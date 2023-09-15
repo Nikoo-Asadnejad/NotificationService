70543592c0c57141d5db43df518144f682804964
Nikoo Asadnejad <nikoo.a.14@gmail.com>
2023-09-15 11:15:01 +0330

    test(notificationRequestTest) : add sendPushMessageRequestShouldBeFilledWithNotificationValues to sendPushNotificationRequestExplicitOprator_tests

--- Tests/Contracts/NotificationContract.Tests/Models/SendNotificationRequest.PushMessageRequestExplicitOprator.Tests.cs
+++ Tests/Contracts/NotificationContract.Tests/Models/SendNotificationRequest.PushMessageRequestExplicitOprator.Tests.cs
@@ -13,7 +13,7 @@ public sealed class SendNotificationRequest_PushMessageRequestExplicitOprator_Te
     
     [Theory]
     [MemberData(nameof(TestDataGenerator.GetRequestsWithNullMessage),MemberType = typeof(TestDataGenerator))]
-    public async Task ConvertToSendSmsRequest_MessageIsNull_ThrowsArgumentNullException(SendNotificationRequest request)
+    public async Task ConvertToSendPushMessageRequest_MessageIsNull_ThrowsArgumentNullException(SendNotificationRequest request)
     {
         Func<SendPushMessageRequest> convertFunc = () => (SendPushMessageRequest)request;
         Exception exception = Assert.Throws<ArgumentNullException>(convertFunc);
@@ -22,7 +22,7 @@ public sealed class SendNotificationRequest_PushMessageRequestExplicitOprator_Te
     
     [Theory]
     [MemberData(nameof(TestDataGenerator.GetRequestsWithNullTitle),MemberType = typeof(TestDataGenerator))]
-    public async Task ConvertToSendSmsRequest_TitleIsNull_ThrowsArgumentNullException(SendNotificationRequest request)
+    public async Task ConvertToSendPushMessageRequest_TitleIsNull_ThrowsArgumentNullException(SendNotificationRequest request)
     {
         Func<SendPushMessageRequest> convertFunc = () => (SendPushMessageRequest)request;
         Exception exception = Assert.Throws<ArgumentNullException>(convertFunc);
@@ -31,10 +31,22 @@ public sealed class SendNotificationRequest_PushMessageRequestExplicitOprator_Te
     
     [Theory]
     [MemberData(nameof(TestDataGenerator.GetRequestsWithNullDeviceToken),MemberType = typeof(TestDataGenerator))]
-    public async Task ConvertToSendSmsRequest_DeviceTokenIsNull_ThrowsArgumentNullException(SendNotificationRequest request)
+    public async Task ConvertToSSendPushMessageRequest_DeviceTokenIsNull_ThrowsArgumentNullException(SendNotificationRequest request)
     {
         Func<SendPushMessageRequest> convertFunc = () => (SendPushMessageRequest)request;
         Exception exception = Assert.Throws<ArgumentNullException>(convertFunc);
         Assert.Contains("DeviceToken" ,exception.Message);
+    }
+    
+    [Fact]
+    public async Task ConvertToSendPushMessageRequest_OnConvert_PushMessageRequestShouldBeFilledWithNotificationValues()
+    {
+        SendNotificationRequest notificationRequest = TestDataGenerator.CreateSampleRequest();
+
+        SendPushMessageRequest pushMessageRequestRequest = (SendPushMessageRequest)notificationRequest;
+        
+        Assert.Equal(notificationRequest.Message , pushMessageRequestRequest.Message);
+        Assert.Equal(notificationRequest.Title , pushMessageRequestRequest.Title);
+        Assert.Equal(notificationRequest.DeviceToken , pushMessageRequestRequest.DeviceToken);
     }
 }
