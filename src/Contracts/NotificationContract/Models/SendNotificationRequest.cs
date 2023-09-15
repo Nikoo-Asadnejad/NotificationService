f0060a7d624ab7286f1af23f3c5e4b0edb27ade4
Nikoo Asadnejad <nikoo.a.14@gmail.com>
2023-09-15 10:17:58 +0330

    refactor(notificationContract) : replace null checks with string null or empty

--- src/Contracts/NotificationContract/Models/SendNotificationRequest.cs
+++ src/Contracts/NotificationContract/Models/SendNotificationRequest.cs
@@ -16,16 +16,16 @@ public sealed record SendNotificationRequest
 
     public static explicit operator SendEmailRequest(SendNotificationRequest notification)
     {
-        if (notification.Email is null)
+        if (string.IsNullOrWhiteSpace(notification.Email))
             throw new ArgumentNullException("Email",message:"For sending an email, email field shouldn't be null");
         
-        if (notification.Message is null)
+        if (string.IsNullOrWhiteSpace(notification.Message))
             throw new ArgumentNullException("Message",message:"For sending an email, message field shouldn't be null");
         
-        if (notification.ReceptorName is null)
+        if (string.IsNullOrWhiteSpace(notification.ReceptorName))
             throw new ArgumentNullException("ReceptorName",message:"For sending an email, receptorName field shouldn't be null");
         
-        if (notification.Title is null)
+        if (string.IsNullOrWhiteSpace(notification.Title))
             throw new ArgumentNullException("Title",message:"For sending an email, title field shouldn't be null");
 
         SendEmailRequest emailRequest = new(notification.Email,notification.ReceptorName, notification.Title, notification.Message);
@@ -33,10 +33,10 @@ public sealed record SendNotificationRequest
     }
     public static explicit operator SendSmsRequest(SendNotificationRequest notification)
     {
-        if (notification.PhoneNumber is null)
+        if (string.IsNullOrWhiteSpace(notification.PhoneNumber))
             throw new ArgumentNullException("PhoneNumber",message:"For sending a sms, phoneNumber field shouldn't be null");
     
-        if (notification.Message is null)
+        if (string.IsNullOrWhiteSpace(notification.PhoneNumber))
             throw new ArgumentNullException("Message",message:"For sending a sms, message field shouldn't be null");
 
         SendSmsRequest smsRequest = new(notification.PhoneNumber, notification.Message);
@@ -44,13 +44,13 @@ public sealed record SendNotificationRequest
     }
     public static explicit operator SendPushMessageRequest(SendNotificationRequest notification)
     {
-        if (notification.DeviceToken is null)
+        if (string.IsNullOrWhiteSpace(notification.DeviceToken))
             throw new ArgumentNullException("DeviceToken",message:"For sending a push message, deviceToken field shouldn't be null");
     
-        if (notification.Title is null)
+        if (string.IsNullOrWhiteSpace(notification.Title))
             throw new ArgumentNullException("Title",message:"For sending a push message, title field shouldn't be null");
         
-        if (notification.Message is null)
+        if (string.IsNullOrWhiteSpace(notification.Message))
             throw new ArgumentNullException("Message",message:"For sending a push message, message field shouldn't be null");
 
         SendPushMessageRequest pushMessageRequestRequest = new(notification.DeviceToken, notification.Title,notification.Message);
