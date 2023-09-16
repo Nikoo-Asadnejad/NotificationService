0d3adc9d2dbc9ebe61a027646c1d9a0732e85911
Nikoo Asadnejad <nikoo.a.14@gmail.com>
2023-09-16 11:46:48 +0330

    feat(notificationService) : implement send notification api

--- src/Services/NotificationService/Controllers/NotificationController.cs
+++ src/Services/NotificationService/Controllers/NotificationController.cs
@@ -1,11 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
+using NotificationContract.Models;
+using NotificationService.Interfaces;
 
 namespace NotificationService.Controllers;
 
 [ApiController]
-[Route("[controller]")]
-public class NotificationController : ControllerBase
+[Route("[controller]/[action]")]
+public class NotificationsController : ControllerBase
 {
+    private readonly INotificationService _notificationService;
 
-    
+    public NotificationsController(INotificationService notificationService)
+    {
+        _notificationService = notificationService;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Send(SendNotificationRequest request)
+    {
+        await _notificationService.SendAsync(request);
+        return Ok();
+    }
+
 }
