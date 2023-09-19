64b5bc6d391871780afe543fc54eca736cece55a
Nikoo Asadnejad <nikoo.a.14@gmail.com>
2023-09-19 21:40:14 +0330

    feat(smsService) : add serilog

--- src/Services/SmsService/Ioc/Configurator.cs
+++ src/Services/SmsService/Ioc/Configurator.cs
@@ -1,5 +1,9 @@
+using System.Reflection;
 using MassTransit;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.Elasticsearch;
 using SmsService.Interfaces;
 using SmsService.Services;
 using SmsContract.Enums;
@@ -22,6 +26,8 @@ public static class Configurator
         var appSetting = new AppSetting();
         configuration.Bind(appSetting);
         Configuration.AppSetting = appSetting;
+        
+        AddLogging(configuration);
     }
 
     public static void AddRabbitConsumer(this IServiceCollection services)
@@ -58,5 +64,25 @@ public static class Configurator
 
             endpoints.MapHealthChecks("/health/live", new HealthCheckOptions());
         });
+    }
+    
+    private static void AddLogging(IConfiguration configuration)
+    {
+        string environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            .Enrich.FromLogContext()
+            .Enrich.WithEnvironmentName()
+            .Enrich.WithMachineName()
+            .WriteTo.Console()
+            .WriteTo.Debug()
+            .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(Configuration.AppSetting.ElasticSearch.Uri))
+            {
+                AutoRegisterTemplate = true,
+                AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv6,
+                IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name!.ToLower().Replace(".", "-")}-{environment?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}"
+            })
+            .ReadFrom.Configuration(configuration)
+            .CreateLogger();
     }
 }
