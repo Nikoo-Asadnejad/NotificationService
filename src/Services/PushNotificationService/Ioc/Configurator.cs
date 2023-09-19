4a706f2dedbdbc0e1418928450eaebc42c26db42
Nikoo Asadnejad <nikoo.a.14@gmail.com>
2023-09-19 21:39:27 +0330

    feat(pushnotificationService) : add serilog

--- src/Services/PushNotificationService/Ioc/Configurator.cs
+++ src/Services/PushNotificationService/Ioc/Configurator.cs
@@ -1,7 +1,11 @@
+using System.Reflection;
 using FirebaseAdmin;
 using Google.Apis.Auth.OAuth2;
 using MassTransit;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.Elasticsearch;
 
 namespace PushNotificationService.Configurations;
 
@@ -19,6 +23,8 @@ public static class SmsConfigurator
         {
             Credential = GoogleCredential.FromFile("firebase.json")
         });
+
+        AddLogging(configuration);
     }
     
     public static void AddRabbitConsumer(this IServiceCollection services)
@@ -40,5 +46,25 @@ public static class SmsConfigurator
 
             endpoints.MapHealthChecks("/health/live", new HealthCheckOptions());
         });
+    }
+    
+    private static void AddLogging(IConfiguration configuration)
+    {
+        string environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            .Enrich.FromLogContext()
+            .Enrich.WithEnvironmentName()
+            .Enrich.WithMachineName()
+            .WriteTo.Console()
+            .WriteTo.Debug()
+            .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(Configuration.AppSetting.ElasticSearch.Uri))
+            {
+                AutoRegisterTemplate = true,
+                AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv6,
+                IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name!.ToLower().Replace(".", "-")}-{environment?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}"
+            })
+            .ReadFrom.Configuration(configuration)
+            .CreateLogger();
     }
 }
